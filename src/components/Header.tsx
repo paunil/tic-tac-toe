6d29f68bef8382e6a94da6a3b3616559e0e88014
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-07-19 23:31:44 +0200

    implement Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,14 @@
-function Header() {
+import '../scss/header.scss'
+
+function Header(props: {currentPlayer: string}) {
   return (
     <div className="score-container">
+      <div className="score">
+        <span>Next player:</span> {props.currentPlayer}
+      </div>
+      <button onClick={() => {window.location.reload()}}>
+        RESTART
+      </button>
     </div>
   )
 }
