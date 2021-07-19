85f94ac1589deca1541557175b58b22b760c934b
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-07-19 23:35:45 +0200

    implement App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Board from './Board'
 import Header from './Header'
 
 
 function App() {
 
+  const [currentPlayer, setCurrentPlayer] = useState<string>('x')
+
+  const switchPlayer = () => {
+    if (currentPlayer === 'x') {
+      setCurrentPlayer('o')
+    } else {
+      setCurrentPlayer('x')
+    }
+  }
+
   return (
     <div className="app-container">
-      <Header/>
-      <Board/>
+      <Header currentPlayer={currentPlayer}/>
+      <Board currentPlayer={currentPlayer} switchPlayer={switchPlayer}/>
     </div>
   )
 }
