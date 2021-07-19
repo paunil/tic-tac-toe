2692ea4cbe2f3b25299b5c8c75b21705a52e3faf
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-07-19 23:33:55 +0200

    implement Square

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,7 +1,12 @@
-function Square() {
-  
+import "../scss/square.scss"
+
+function Square(props: {handleOnClick: React.MouseEventHandler<HTMLButtonElement>, selected: string}) {
+  const imageSrc = `./images/${props.selected ? props.selected : 'square'}.png`
   return (
     <div className="square">
+      <button onClick={props.handleOnClick}>
+        <img src={imageSrc} alt="square_image"></img>
+      </button>
     </div>
   )
 }
